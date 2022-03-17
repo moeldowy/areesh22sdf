1bd24ada4dccbe070e7c2be58c720b77a0b49ed6
Mohammed Salah Eldowy <moeldowy@iti.gov.eg>
2022-03-17 11:35:08 +0200

    First Start

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo "Welcome From PHP server"?>
+<?php
+/*
+ * to make interpreter skip one line use // or #
+ * to make interpreter skip block of code use at first /* and at the end of block use */
+ /* */
+//echo 'WELCOME FROM ECHO';
+//echo ("ghjgjghjghjghj");
+/*print 'WELCOME FROM PRINT';
+print("ghjgjghjghjghj");*/
+//echo"hi";
+/*
+ * echo is faster than print
+ * echo can print more than one value
+ * but print can only print one value
+ * */
+//echo"ali","ahmed","ashraf";
+//print"ali","ahmed","ashraf";
